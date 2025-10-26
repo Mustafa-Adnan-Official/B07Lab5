be1cd8ab56ab6aa1c0f06043888f6d95676000d4
prabor06 <prabor09@gmail.com>
2025-10-26 13:37:43 -0400

    feat(pentagon): add Pentagon class with perimeter and isRegular method

--- src/B07Lab5/Pentagon.java
+++ src/B07Lab5/Pentagon.java
@@ -1,1 +1,31 @@
 package B07Lab5;
+
+public class Pentagon {
+	Point A;
+	Point B;
+	Point C;
+	Point D;
+	Point E;
+	
+	public Pentagon(Point A, Point B, Point C, Point D, Point E) {
+		this.A = A;
+        this.B = B;
+        this.C = C;
+        this.D = D;
+        this.E = E;
+	}
+	
+	public double perimeter() {
+		return A.distance(B) + B.distance(C) + C.distance(D) + D.distance(E) + E.distance(A)
+	}
+	
+	public boolean isRegular() {
+		double dAB = A.distance(B);
+        double dBC = B.distance(C);
+        double dCD = C.distance(D);
+        double dDE = D.distance(E);
+        double dEA = E.distance(A);
+
+        return dAB == dBC && dBC == dCD && dCD == dDE && dDE == dEA;
+    }	
+}
